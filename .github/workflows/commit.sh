e70748ebfeb6921e8d5dfa4b3afcdef136561855
notusknot <notusknot@gmail.com>
2022-01-23 21:36:11 -0800

    Test new action

---
+++ .github/workflows/commit.sh
@@ -0,0 +1,6 @@
+COMMIT=$(git rev-parse --short HEAD)
+PREV=$(git rev-parse --short HEAD^)
+URL="https://github.com/notusknot/notusknot.gihub.io/commit/$COMMIT"
+MESSAGE="Powered by git commit $COMMIT"
+
+sed -i "s/$PREV/$COMMIT/g" ../../index.html
