0a9f547e58623ff5c3ce5e5b41c5207093eba911
notusknot <notusknot@gmail.com>
2022-01-23 22:00:38 -0800

    This is stupidly overengineered in order to be perfectly minimalist

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -3,5 +3,8 @@ PREV=$(git rev-parse --short HEAD^)
 URL="https://github.com/notusknot/notusknot.gihub.io/commit/$COMMIT"
 MESSAGE="Powered by git commit $COMMIT"
 
-pwd
 sed -i "s/$PREV/$COMMIT/g" ./index.html
+
+git add .
+git commit -m "Updated footer with commit hash"
+git push origin main
