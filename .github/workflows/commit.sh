e70d169591dac749203df96d02d3b3bd14092288
notusknot <notusknot@gmail.com>
2022-01-23 21:44:14 -0800

    pwd

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -3,4 +3,5 @@ PREV=$(git rev-parse --short HEAD^)
 URL="https://github.com/notusknot/notusknot.gihub.io/commit/$COMMIT"
 MESSAGE="Powered by git commit $COMMIT"
 
+pwd
 sed -i "s/$PREV/$COMMIT/g" ./index.html
