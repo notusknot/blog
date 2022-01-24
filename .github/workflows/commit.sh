7a2dcb7e1b4e287b9b3ef602f06753b1296655a8
notusknot <notusknot@gmail.com>
2022-01-23 22:11:33 -0800

    pls just work

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -1,10 +1,8 @@
+git checkout gh-pages
+
 COMMIT=$(git rev-parse --short HEAD)
 PREV=$(git rev-parse --short HEAD^)
 URL="https://github.com/notusknot/notusknot.gihub.io/commit/$COMMIT"
 MESSAGE="Powered by git commit $COMMIT"
 
-sed -i "s/$PREV/$COMMIT/g" ./index.html
-
-git add .
-git commit -m "Updated footer with commit hash"
-git push origin main
+sed -i "s/88209ac/$COMMIT/g" ./index.html
