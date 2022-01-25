7910559d4a66ccaea39109ba4be1e2082169f7ce
notusknot <notusknot@gmail.com>
2022-01-25 10:12:44 -0800

    Small commit script fix again part 6

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -3,5 +3,5 @@ PREV=$(git rev-parse --short HEAD^)
 URL="https://github.com/notusknot/notusknot.gihub.io/commit/$COMMIT"
 MESSAGE="Powered by git commit $COMMIT.</a>"
 sed -i "s#<a href=https://github.com/notusknot/notusknot.github.io/commit/.*/ target=_blank>#
-<a href=https://github.com/notusknot/notusknot.github.io/commit/a1b2c3/ target=_blank>#g" ./index.html
+<a href=https://github.com/notusknot/notusknot.github.io/commit/$COMMIT target=_blank>#g" ./index.html
 sed -i "s#Powered by git commit.*.</a>#Powered by git commit $COMMIT.</a>#g" ./index.html
