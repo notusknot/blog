fc0c96a7a21d0b8a8772c5fe75501189487ceb15
notusknot <notusknot@gmail.com>
2022-02-04 13:20:28 -0800

    Change repo name

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -2,7 +2,7 @@ COMMIT=$(git rev-parse --short HEAD)
 PREV=$(git rev-parse --short HEAD^)
 URL="https://github.com/notusknot/notusknot.gihub.io/commit/$COMMIT"
 MESSAGE="Powered by git commit $COMMIT.</a>"
-sed -i "s#<a href=https://github.com/notusknot/notusknot.github.io/commit/.*/ target=_blank>#<a href=https://github.com/notusknot/notusknot.github.io/commit/$COMMIT/ target=_blank>#g" ./index.html
+sed -i "s#<a href=https://github.com/notusknot/blog/commit/.*/ target=_blank>#<a href=https://github.com/notusknot/blog/commit/$COMMIT/ target=_blank>#g" ./index.html
 echo "sed $COMMIT"
 cat index.html
 sed -i "s#Powered by git commit.*.</a>#Powered by git commit $COMMIT.</a>#g" ./index.html
