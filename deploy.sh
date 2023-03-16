d0e4ff88d0fc7b446e61ee4a002ca662a48476c2
notusknot <notusknot@gmail.com>
2023-03-15 17:50:15 -0700

    Test

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+directory=public
+branch=gh-pages
+build_command() {
+  hugo --minify
+}
+
+echo -e "\033[0;32mDeleting old content...\033[0m"
+rm -rf $directory
+
+echo -e "\033[0;32mGenerating site...\033[0m"
+build_command
+
+echo -e "\033[0;32mDeploying $branch branch...\033[0m"
+cd $directory &&
+  git add --all &&
+  git commit -m "Deploy updates" &&
+  git push origin $branch
